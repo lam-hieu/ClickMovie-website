cd2795ed337f589943735a90c70259f51bb18de4
Trung-Hieu Lam <hieulam0918@gmail.com>
2022-08-22 10:15:28 +0700

    fix sth

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,13 @@ const arrows = $$('.arrow-right')
 const movieList = $$('.movie-list')
 
 arrows.forEach((arrow, index) => {
-    const itemNumber = movieList[index].querySelectorAll("img").length;
-    console.log(itemNumber)
+    const itemNumber = movieList[index].querySelectorAll("img").length; //đến số phần từ mảng trong mảng (phim trong listphim)
+    // console.log(itemNumber)
     let clickCounter = 0;
     arrow.addEventListener('click', () => {
+        const ration = Math.floor(window.innerWidth / 320) //img discrease follow width / size img
         clickCounter++;
-        if (itemNumber - (4 + clickCounter) >= 0) {
+        if ((itemNumber - (5 + clickCounter) + (5 - ration)) >= 0) {
             movieList[index].style.transform = `
                 translateX(${movieList[index].computedStyleMap().get('transform')[0].x.value - 400}px)`
         } else {
@@ -18,7 +19,8 @@ arrows.forEach((arrow, index) => {
             clickCounter = 0;
         }
 
-    })
+    });
+    console.log(window.innerWidth)
 });
 
 
